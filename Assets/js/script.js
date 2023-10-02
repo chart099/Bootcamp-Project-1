0c094eb330b9497112df8d10f6296e8ee0bd8a81
cj cabrera <cabrera012500@gmail.com>
2023-10-02 13:50:56 -0500

    added displayEvents function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 const apiKey = 'iIm9cRrzUWIOEnRZYIrJy0Adv7GdRjad';
 const apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
-const artistName = 'Drake';  // Replace with the artist you're interested in
+const artistName = '';  // Replace with the artist you're interested in
 
 const fetchEvents = () => {
   const url = `${apiUrl}?keyword=${artistName}&apikey=${apiKey}`;
@@ -15,9 +15,36 @@ const fetchEvents = () => {
     })
     .then(data => {
       // Process and display the event data
-      console.log('Event data:', data);
+      displayEvents(data._embedded?.events);
     })
     .catch(error => console.error('Error fetching data:', error));
+};
+
+const displayEvents = (events) => {
+  const eventDisplay = document.getElementById('event-display');
+  eventDisplay.innerHTML = '';  // Clear previous events
+
+  if (events) {
+    events.forEach(event => {
+      const eventCard = document.createElement('div');
+      eventCard.classList.add('card');
+
+      const eventName = document.createElement('div');
+      eventName.classList.add('card-header');
+      eventName.textContent = event.name;
+
+      const eventDate = document.createElement('div');
+      eventDate.classList.add('card-content');
+      eventDate.textContent = `Date: ${event.dates.start.localDate}`;
+
+      eventCard.appendChild(eventName);
+      eventCard.appendChild(eventDate);
+
+      eventDisplay.appendChild(eventCard);
+    });
+  } else {
+    eventDisplay.textContent = 'No events found for the artist.';
+  }
 };
 
 // Call the function to fetch events
